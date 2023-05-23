7b6efaba470e656d775ed2d0fdb116001aef05b0
HumbleG0d <sergiouni2000@gmail.com>
2023-05-23 13:57:12 -0500

    add the functionality of being able to delete what is written in the input

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import { OPERATIONS } from './operation.js'
 
 const getTextInput = () => {
   const input = document.getElementById('calulator__input')
-  let text
+  let text 
   input.addEventListener('keyup', () => {
     text = input.value
     const reg = /\s/g
@@ -11,33 +11,51 @@ const getTextInput = () => {
     //*split(/(\d+)/) nos permite separar la cadena por numero pero sin perder dicho numero , esto gracias al agrupamiento ().
     text = text.split(/(\d+)/).filter(char => char != '')
   })
-  
-  //* [1 , + , 2 , + , 3 , + , 4 , + , 5]
-  //!  0   1   2   3   4   5   6   7   8
-  //? Vemos a que tipo de operacion se refiere y hacemos un llamodo a la funcion respectiva. Pero esto se ejecutara al hacer enter o click en el boton igual
+
+  //* Al precionar el boton igual , se calcula.
   const equal = document.getElementById('equal')
   equal.addEventListener('click' , () => {
-    let operation
-    let a
-    const operation1 = document.getElementById('operation__1')
-    operation1.innerHTML = ' '
-    if(text[text.length - 1] === TYPE_OPERATIONS.ADD){
-      operation1.innerHTML = 'ERROR'
-      operation1.style.color = 'red'
-    }
-    else{
-      for (let i = 0 ; i < text.length ; i++) {
-        if(i === 0) a = parseInt(text[i])
-        if(text[i] === TYPE_OPERATIONS.ADD){
-          operation = OPERATIONS.add(a , parseInt(text[i + 1]))
-          a = operation
-        }
-      }
-      operation1.innerHTML = `${a}`
-      operation1.style.color = 'gray'
-    }
-  
+    performCalculation(text)
   })
+
+  //* Al precionar la tecla enter , se calcula
+  document.addEventListener('keydown' , (e) => {
+    if(e.key === 'Enter' || e.key === 'Return'){
+      performCalculation(text)
+    }
+  })
+
+  //* Borramos el contenido del input.
+  const button_c = document.getElementById('number_c')
+  button_c.addEventListener('click' , () => {
+    input.value = ' '
+  })
+}
+
+//* Esta funcion se encargara de hacer los respectivos calculos
+function performCalculation(text){
+  //* [1 , + , 2 , + , 3 , + , 4 , + , 5]
+  //!  0   1   2   3   4   5   6   7   8
+  let operation
+  let a
+  const operation1 = document.getElementById('operation__1')
+  operation1.innerHTML = ' '
+  if(text[text.length - 1] === TYPE_OPERATIONS.ADD){
+    operation1.innerHTML = 'ERROR'
+    operation1.style.color = 'red'
+  }
+  else{
+    for (let i = 0 ; i < text.length ; i++) {
+      if(i === 0) a = parseInt(text[i])
+      if(text[i] === TYPE_OPERATIONS.ADD){
+        operation = OPERATIONS.add(a , parseInt(text[i + 1]))
+        a = operation
+      }
+    }
+    operation1.innerHTML = `${a}`
+    operation1.style.color = 'gray'
+  }
+
 }
 
 getTextInput()
