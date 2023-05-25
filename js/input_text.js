63e89bd07fe361a3353ede3d31b4e83a13846485
HumbleG0d <sergiouni2000@gmail.com>
2023-05-25 02:55:07 -0500

    add the necessary code to be able to detect the decimal point

--- js/input_text.js
+++ js/input_text.js
@@ -2,6 +2,15 @@ export function getInputValue(text){
   const reg = /\s/g
   text = text.replace(reg, '')
   //*split(/(\d+)/) nos permite separar la cadena por numero pero sin perder dicho numero , esto gracias al agrupamiento ().
-  text = text.split(/(\d+)/).filter(char => char != '')
+  
+  //*text.split(/(\.-|\+|\*|\/)/g).filter(char => char != '')
+
+  if(text.length >= 3){
+    text = text.split(/(\.-|\+|\x|\/)/g).filter(char => char != '')
+  }
+  else{
+    text = text.split(/(\d+)/).filter(char => char != '')
+  }
+
   return text
 }
